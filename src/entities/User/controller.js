73a16cbe4ed84af066ae90f157485c3a69e51535
VoitV <v.voitenko123@gmail.com>
2024-07-16 12:11:16 +0300

    add binding to backend counter

--- src/entities/User/controller.js
+++ src/entities/User/controller.js
@@ -1,5 +1,6 @@
 import db from '../../db/db.js';
 import bcrypt from 'bcryptjs';
+import { use } from 'chai';
 import jwt from 'jsonwebtoken'
 
 const generateAcessToken = (id) => {
@@ -28,7 +29,7 @@ export const createUser = async (req, res) => {
             password: hashPassword
         }).returning('*').then(data=>data[0])
 
-        const token = generateAcessToken(user.user_id);
+        const token = generateAcessToken(user.id);
 
         res.json(token);
     } catch (error) {
@@ -39,9 +40,8 @@ export const createUser = async (req, res) => {
 
 
 export const loginUser = async (req,res) => {
-    const { email, password  } = req.body;
-
-    const user = await db.select('*').from("user").where({email}).then(data=> data[0]);
+    const { login, password  } = req.body;
+    const user = await db.select('*').from("user").where({login}).then(data=> data[0]);
 
     if(!user) {
         return res.status(400).json({message:'User not found',errorCode:400})
@@ -55,7 +55,7 @@ export const loginUser = async (req,res) => {
     }
 
 
-    const token = generateAcessToken(user.user_id)
+    const token = generateAcessToken(user.id)
 
     res.json(token)
 }
@@ -77,5 +77,115 @@ export const getUsers = async (req,res) => {
     const users = await db.select('*').from("user");
 
     res.json(users);
+}
+
+export const getUserBalance = async (req, res) => {
+    if (!req.user) {
+        return res.status(400).json({ message: 'User not found', errorCode: 400 });
+    }
+
+    const id = req.user.id;
+
+    try {
+        const user = await db.select('*').from("user").where({ id }).first();
+        if (!user) {
+            return res.status(404).json({ message: 'User not found', errorCode: 404 });
+        }
+        return user.balance;
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error', errorCode: 500 });
+    }
+}
+
+export const getUserBalanceSync = async (req, res) => {
+    if (!req.user) {
+        return res.status(400).json({ message: 'User not found', errorCode: 400 });
+    }
+
+    const id = req.user.id;
+
+    try {
+        const user = await db.select('*').from("user").where({ id }).first();
+        if (!user) {
+            return res.status(404).json({ message: 'User not found', errorCode: 404 });
+        }
+        res.json(user.balance);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error', errorCode: 500 });
+    }
+}
+
+export const getUserIncome = async (req, res) => {
+    if (!req.user) {
+        return res.status(400).json({ message: 'User not found', errorCode: 400 });
+    }
+
+    const id = req.user.id;
+
+    try {
+        const user = await db.select('*').from("user").where({ id }).first();
+        if (!user) {
+            return res.status(404).json({ message: 'User not found', errorCode: 404 });
+        }
+        return user.income;
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error', errorCode: 500 });
+    }
+}
+
+export const getUserGameInfo = async (req, res) => {
+    try {
+        const balancePromise = getUserBalance(req, res);
+        const incomePromise = getUserIncome(req, res);
+
+        const [balance, income] = await Promise.all([balancePromise, incomePromise]);
+
+        if (typeof balance === 'number' && typeof income === 'number') {
+            return res.json({ balance, income });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error', errorCode: 500 });
+    }
+}
+
+export const changeBalance = async (req, res) => {
+    if (!req.user) {
+        return res.status(400).json({ message: 'User not found', errorCode: 400 });
+    }
+
+    const id = req.user.id;
+    const { operation, number } = req.body;
+
+    if (!operation || typeof number !== 'number') {
+        return res.status(400).json({ message: 'Invalid operation or number', errorCode: 400 });
+    }
+
+    try {
+        const userBalance = await getUserBalance(req, res);
+
+        let newUserBalance;
+
+        switch (operation) {
+            case 'add':
+                newUserBalance = userBalance + number;
+                break;
+            case 'subtract':
+                newUserBalance = userBalance - number;
+                break;
+            default:
+                return res.status(400).json({ message: 'Invalid operation', errorCode: 400 });
+        }
+
+        await db('user').where({ id }).update({ balance: newUserBalance });
+
+        return res.json({ balance: newUserBalance });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error', errorCode: 500 });
+    }
 }
 
--- src/entities/User/routes.js
+++ src/entities/User/routes.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 import { createUser,
     loginUser,
     getUser, 
-    getUsers
+    getUsers,
+    getUserBalance,
+    getUserGameInfo,
+    changeBalance,
+    getUserBalanceSync
  } from "./controller.js";
 import { handleErrorWrap } from "../../middlewares/errorMiddleware.js";
 import { authMiddleware } from "../../middlewares/authMiddleware.js";
@@ -10,6 +14,9 @@ import { authMiddleware } from "../../middlewares/authMiddleware.js";
 export const userRouter = new Router();
 
 userRouter.post('/create-user',handleErrorWrap(createUser));
-userRouter.post('/login-user',handleErrorWrap(loginUser));
+userRouter.post('/login',handleErrorWrap(loginUser));
 userRouter.get('/user',authMiddleware, handleErrorWrap(getUser));
 userRouter.get('/users',handleErrorWrap(getUsers));
+userRouter.get('/balance',authMiddleware, handleErrorWrap(getUserBalanceSync));
+userRouter.get('/game-info',authMiddleware,handleErrorWrap(getUserGameInfo));
+userRouter.patch('/balance-change',authMiddleware,handleErrorWrap(changeBalance));
